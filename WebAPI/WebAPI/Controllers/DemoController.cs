62a08736a0a023f5b9587d0f4aab5d639bdb4cfe
KamalVarma <varmakamalrajendra@gmail.com>
2022-08-02 17:29:38 +0530

    WebAPI

---
+++ WebAPI/WebAPI/Controllers/DemoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class DemoController : ApiController
+    {
+        static List<string> mystrings = new List<string> { "value0", "value1", "value2" };
+
+        public IEnumerable<string> Get()
+        {
+            return mystrings;
+        }
+
+        public string Get(int id)
+        {
+            return mystrings[id];
+        }
+
+        public IEnumerable<string> Post([FromUri] Class1 val)
+        {
+            mystrings.Add(val.name);
+            return mystrings;
+        }
+
+        //public IEnumerable<string> post([FromBody] string val)
+        //{
+        //    mystrings.Add(val);
+        //    return mystrings;
+        //}
+
+        public IEnumerable<string> Put(int id,[FromUri]string val)
+        {
+            mystrings[id - 1] = val;
+            return mystrings;
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [RoutePrefix("api/Person")]
+    public class PersonController : ApiController
+    {
+        static List<Person> personlist = new List<Person>()
+        {
+            new Person{id=1,name="kamal",city="Pune"},
+            new Person{id=2,name="varma",city="Mumbai"},
+            new Person{id=3,name="RK",city="Jaipur"},
+        };
+        [HttpGet]
+        [Route("PersonDetails")]
+
+        public IEnumerable<Person> Get()
+        {
+            return personlist;
+        }
+
+        [HttpGet]
+        [Route("personlist")]
+
+        public HttpResponseMessage GetPersonList()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK,personlist);
+            return response;
+        }
+
+        [Route("p")]
+        public HttpResponseMessage GetP(int pid)
+        {
+            var person = personlist.Find(x => x.id == pid);
+            if (person == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, pid);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, person);
+        }
+
+        //Post
+
+        public Person POst([FromBody]Person p)
+        {
+            personlist.Add(p);
+            return p;
+        }
+    }
+}
